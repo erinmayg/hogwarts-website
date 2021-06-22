f33f777a4148f04c2c472b9a26bb795bdd138a24
erinmayg <erinmaygunawan@gmail.com>
2021-06-23 02:40:47 +0800

    Cursor mask effect

--- src/components/HogwartsInfo.jsx
+++ src/components/HogwartsInfo.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import '../scss/hogwartsInfo.scss';
 import hogwarts from '../images/hogwartsPurple.png';
+import hogwartsHaunted from '../images/hogwartsHaunted.gif';
 import { ReactComponent as Deco } from '../svg/deco.svg';
 
 function HogwartsInfo() {
-  return (
-    <div className='hogwartsInfo fullscreen page'>
+  const hogwartsInfo = (
+    <div className='hogwartsInfo fullscreen'>
       <div className='hogwartsInfo--pic'>
         <img src={hogwarts} />
         <div className='hogwartsInfo--pic--text'>
@@ -30,6 +31,40 @@ function HogwartsInfo() {
           old, abandoned castle.
         </p>
       </div>
+    </div>
+  );
+
+  const hogwartsInfoAlt = (
+    <div className='hogwartsInfo--alt fullscreen' aria-hidden={true} data-hero>
+      <div className='hogwartsInfo--pic'>
+        <img src={hogwartsHaunted} />
+        <div className='hogwartsInfo--pic--text'>
+          <h1>Hogwarts</h1>
+          <h2>Scottish Highlands</h2>
+        </div>
+        <Deco />
+      </div>
+      <div className='hogwartsInfo--text'>
+        <Deco />
+        <h1>990 A.D.</h1>
+        <p>
+          Hogwarts is an old abandoned castle located in the Scottish Highlands.
+          It was a British boarding school built around 990 A.D.
+        </p>
+        <p>
+          The exact cause of its ruin remains unknown. But legend says you can
+          hear the ghosts shrieking inside the castle to this day, its
+          supernatural activity most active during the witching hour.
+        </p>
+      </div>
+    </div>
+  );
+  
+
+  return (
+    <div className='fullscreen page'>
+      {hogwartsInfo}
+      {hogwartsInfoAlt}
     </div>
   );
 }
